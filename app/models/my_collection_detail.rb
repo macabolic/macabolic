0eb02fd3a3325279f61dbc6a9e54826b3dfd86d5
Billy Cheng <digitalmacliving@gmail.com>
2010-11-11 03:18:15 +0800

    Revise My_Collection
    - Revise the layout.
    
    
    Signed-off-by: Billy Cheng <digitalmacliving@gmail.com>

--- app/models/my_collection_detail.rb
+++ app/models/my_collection_detail.rb
@@ -14,4 +14,31 @@
 class MyCollectionDetail < ActiveRecord::Base
   belongs_to  :my_collection
   belongs_to  :product
+  
+  def number_of_friends_own_this
+    # 1. user.friends
+    # 2. for each friend, get the list of my_collections and see if my_friend owns the product
+    if product.nil?
+      return 0
+    else
+      @count = 0
+      # Got all my list of friends
+      @friends = my_collection.user.my_friends
+      logger.info "I have #{@friends.size} friends."
+      
+      # For each friend
+      @friends.each do |friend|
+        logger.info "My friend (#{friend.friend.login}) has #{friend.friend.my_collections}"
+        friend.friend.my_collections.each do |my_collection|
+          my_product = MyCollectionDetail.find(:first, :conditions => {:my_collection_id => my_collection.id, :product_id => product.id})
+          if !my_product.nil?
+            @count += 1
+          end          
+        end
+      end
+      
+      return @count
+    end   
+  end
+  
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe HomeController do
+
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'show'" do
+    it "should be successful" do
+      get 'show'
+      response.should be_success
+    end
+  end
+end
