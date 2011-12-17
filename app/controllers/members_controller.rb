ec66588a11fc03b2b3a88863154f4ba15f80dafc
Billy Cheng <billy.cheng@macabolic.com>
2011-12-18 01:09:15 +0800

    - Updated "Invitation form" and "Edit profile" page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -51,23 +51,6 @@ class MembersController < ApplicationController
       end
     end
   end
-
-  def update_profile_picture
-    @user = User.find(params[:id])
-
-    # Need to create a new record in "profile_images"
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(edit_member_path(@user), :notice => "Your info was successfully updated.") }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-    
-  end
   
   # DELETE /members/1
   # DELETE /members/1.xml
