9cce0d960452b3e2f762145bbb1e756b611439dd
Billy Cheng <billy.cheng@macabolic.com>
2012-01-18 16:28:10 +0800

    Remove the domain information from the invitation mail.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,7 @@ class UserMailer < ActionMailer::Base
     @subject = "You are invited to join Macabolic"
     @to = invitation.recipient_email
     @reminder = reminder
+    @domain = "teaser.macabolic.com"
     
     mail( :from => "The Macabolic Team <general.support@macabolic.com>", :to => @to, :subject => @subject) do |format|
       format.html { render 'invitation' }
